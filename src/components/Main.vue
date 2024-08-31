0814559302e4d8d32be2f4ec5bb2f3b5ea3905cf
SimoneDUrso <simone.durso98@gmail.com>
2024-08-31 12:26:39 +0200

    [UPD] Created fifth section of main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -185,6 +185,70 @@ export default {
                 </div>
             </div>
         </div>
+
+        <!-- QUINTA SEZIONE -->
+        <div class="container p-5">
+            <div class="row p-5">
+                <div class="col-6">
+                    <span class="fw-bold fs-2">Creative Leader</span><span class="text-danger fw-bolder fs-2">.</span>
+                    <hr>
+                    <p class="lorem">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia quisquam, nemo fugiat asperiores veniam molestiae accusamus facilis modi explicabo perferendis quasi quae, possimus id sed. Provident sit doloremque vitae consectetur!</p>
+                    <div class="py-4 read-more fw-bold">
+                        READ MORE
+                    </div>
+                </div>
+        
+                <div class="col-6">
+                    <!-- Primo Blocco Mentorship -->
+                    <div class="row border-bottom position-relative">
+                        <div class="col-10 p-0">
+                            <span class="fs-4"><strong>Mentorship</strong></span>
+                        </div>
+                        <div class="col-2 p-0 text-end">
+                            <span class="fs-4"><strong>78%</strong></span>
+                        </div>
+                        <!-- Barra di percentuale -->
+                        <div class="percent-bar" style="width: 78%;"></div>
+                    </div>
+        
+                    <!-- Secondo Blocco Mentorship -->
+                    <div class="row border-bottom position-relative my-4">
+                        <div class="col-10 p-0">
+                            <span class="fs-4"><strong>Education</strong></span>
+                        </div>
+                        <div class="col-2 p-0 text-end">
+                            <span class="fs-4"><strong>95%</strong></span>
+                        </div>
+                        <!-- Barra di percentuale -->
+                        <div class="percent-bar" style="width: 95%;"></div>
+                    </div>
+        
+                    <!-- Terzo Blocco Mentorship -->
+                    <div class="row border-bottom position-relative my-4">
+                        <div class="col-10 p-0">
+                            <span class="fs-4"><strong>Learning</strong></span>
+                        </div>
+                        <div class="col-2 p-0 text-end">
+                            <span class="fs-4"><strong>65%</strong></span>
+                        </div>
+                        <!-- Barra di percentuale -->
+                        <div class="percent-bar" style="width: 65%;"></div>
+                    </div>
+        
+                    <!-- Quarto Blocco Mentorship -->
+                    <div class="row border-bottom position-relative">
+                        <div class="col-10 p-0">
+                            <span class="fs-4"><strong>Motivation</strong></span>
+                        </div>
+                        <div class="col-2 p-0 text-end">
+                            <span class="fs-4"><strong>83%</strong></span>
+                        </div>
+                        <!-- Barra di percentuale -->
+                        <div class="percent-bar" style="width: 83%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -295,6 +359,32 @@ main{
 
         .fa-regular, .fa-solid{
             color: #FF4612;
+        }
+    }
+
+    .container .col-6{
+        hr {
+            border: none;
+            height: 2px;
+            width: 100px;
+            color: #FF4612;
+            background-color: #FF4612;
+        }
+        .read-more{
+            height: 3px;
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+        }
+        .percent-bar {
+            position: absolute;
+            bottom: -2px;
+            left: 0;
+            height: 4px;
+            background-color: #dc3545;
+        }
+        .lorem{
+            font-size: 18px;
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap'
+import '@splidejs/vue-splide/css';
 
 createApp(App).mount('#app')
