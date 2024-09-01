7dbddd0cac92371fabf6959dc04d2ab08bf41e82
SimoneDUrso <simone.durso98@gmail.com>
2024-09-01 17:10:07 +0200

    [UPD] Created another component for another carousel

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@ import imageSlider from './imageSlider.vue';
 import fourthSection from './fourthSection.vue';
 import latestNews from './latestNews.vue';
 import creativeLeader from './creativeLeader.vue';
+import testimonialsCarousel from './testimonialsCarousel.vue';
 
 export default {
     components:{
@@ -12,6 +13,7 @@ export default {
         fourthSection,
         latestNews,
         creativeLeader,
+        testimonialsCarousel,
     },
 };
 </script>
@@ -67,46 +69,7 @@ export default {
         <latestNews />
 
         <!-- SETTIMA SEZIONE -->
-        <div class="container-seventh-section position-relative">
-            <a href="#"><i class="fa-solid fa-arrow-up-long position-absolute"></i></a>
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="position-relative"><strong>Testimonials.</strong></h2>
-                    <div class="container-testimonials position-absolute">
-                        <div class="row text-white">
-                            <div class="col-2 d-flex align-items-center justify-content-start">
-                                <button class="btn btn-md text-white fs-2"><i class="fa-solid fa-arrow-left-long"></i></button>
-                            </div>
-        
-                            <div class="col-8 text-center">
-                                <img src="../assets/h3-img-04.png" alt="">
-                                <p class="fw-bold fs-4 mt-4">Cynthia Clark</p>
-                                <p class="lorem fs-5">"Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                                    Non quia, temporibus nemo tenetur sapiente doloribus expedita ducimus architecto 
-                                    tempore in qui odio quasi omnis inventore sint perspiciatis id quam animi?"</p>
-                                    
-                                <!-- BARRA DI PROGRESSIONE DEI PROFILI -->
-                                <div class="progress-bar-container mt-4 d-flex align-items-center justify-content-center w-25">
-                                    <span class="progress-bar-start">01</span>
-
-                                    <div class="progress-bar">
-                                        <div class="progress">
-
-                                        </div>
-                                    </div>
-                                    
-                                    <span class="progress-bar-end">03</span>
-                                </div>
-                            </div>
-        
-                            <div class="col-2 d-flex align-items-center justify-content-end fs-2">
-                                <button class="btn btn-md text-white fs-2"><i class="fa-solid fa-arrow-right-long"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <testimonialsCarousel />
 
         <!-- OTTAVA SEZIONE -->
         <div class="container-eighth-section position-relative">
@@ -176,31 +139,20 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-main{
+main {
     height: 100%;
     overflow-x: hidden;
 
-    .lorem{
+    .lorem {
         color: #BDBDBD;
     }
-    .container.my-5.position-relative{
-        .fa-arrow-up-long{
-            right: -280px;
-            top: -150px;
-            font-size: 30px;
-            background-color: white;
-            padding: 10px;
-            border-radius: 100%;
-            box-shadow: 5px 10px 8px  #888888;
-            color: #FF4612;
-        }
-    }
 
-    .container-specialties.position-relative{
+    .container-specialties.position-relative {
         width: 1800px;
         margin: 0 auto;
         top: -70px;
         background-color: white;
+
         hr {
             border: none;
             height: 2px;
@@ -210,152 +162,85 @@ main{
         }
     }
 
-    .container-seventh-section{
-        height: 500px;
-        background-color: #1E1E1E;
-        padding: 0;
+    
 
-        .fa-arrow-up-long{
-            right: 20px;
-            top: 80px;
-            font-size: 30px;
-            background-color: white;
-            padding: 10px;
-            border-radius: 100%;
-            box-shadow: 5px 10px 8px  #888888;
-            color: #FF4612;
-            z-index: 10000;
-        }
-
-        h2{
-            font-size: 200px;
-            text-align: center;
-            top: -50px;
-        }
-
-        .col-12{
-            z-index: 1;
-
-            .container-testimonials{
-                width: 1600px;
-                z-index: 10;
-                left: 50%;
-                top: 50%;
-                transform: translate(-50%, -50%);
-
-                .col-8{
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    flex-direction: column;
-
-                    .progress-bar-container {
-                        width: 100%;
-                    }
-                    
-                    .progress-bar {
-                        flex-grow: 1;
-                        height: 3px; 
-                        background-color: #ccc; 
-                        margin: 0 10px;
-                        position: relative;
-                        border-radius: 3px;
-                    }
-                    
-                    .progress {
-                        width: 33%;
-                        height: 100%;
-                        background-color: #fff;
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                        border-radius: 3px;
-                    }
-                    
-                    .progress-bar-start,
-                    .progress-bar-end {
-                        font-weight: bold;
-                        font-size: 1rem;
-                    }
-                }
-            }
-        }
-    }
-    .container-eighth-section{
+    .container-eighth-section {
         height: 900px;
         background-image: url(../assets/h1-img-04.jpg);
         background-size: cover;
         background-position: center;
         position: relative;
 
-        .fa-arrow-up-long{
+        .fa-arrow-up-long {
             right: 20px;
             top: 200px;
             font-size: 30px;
             background-color: white;
             padding: 10px;
             border-radius: 100%;
-            box-shadow: 5px 10px 8px  #888888;
+            box-shadow: 5px 10px 8px #888888;
             color: #FF4612;
         }
 
-    .col-12 {
-        font-size: 3rem;
-        color: #fff;
-        
+        .col-12 {
+            font-size: 3rem;
+            color: #fff;
+        }
     }
-}
-    .container-ninth-section{
+
+    .container-ninth-section {
         height: 600px;
         width: 1200px;
         margin: 200px auto;
 
-        .fa-arrow-up-long{
+        .fa-arrow-up-long {
             right: -340px;
             top: -230px;
             font-size: 30px;
             background-color: white;
             padding: 10px;
             border-radius: 100%;
-            box-shadow: 5px 10px 8px  #888888;
+            box-shadow: 5px 10px 8px #888888;
             color: #FF4612;
         }
-        
-        .col-6.bg-world{
+
+        .col-6.bg-world {
             background-image: url(../assets/h1-contact-rev-01.png);
             background-repeat: no-repeat;
             position: relative;
-            .plus-1{
+
+            .plus-1 {
                 left: 150px;
                 top: 100px;
             }
 
-            .plus-2{
+            .plus-2 {
                 left: 300px;
                 top: 70px;
             }
 
-            .plus-3{
+            .plus-3 {
                 right: 100px;
                 top: 230px;
             }
-            .coaching{
+
+            .coaching {
                 width: 300px;
                 padding: 10px;
                 background-color: white;
                 bottom: 120px;
                 right: 10px;
 
-                .read-more{
+                .read-more {
                     height: 3px;
                     display: flex;
                     align-items: center;
                     cursor: pointer;
                 }
-
             }
         }
-        .col-6{
+
+        .col-6 {
             hr {
                 border: none;
                 height: 2px;
@@ -363,34 +248,38 @@ main{
                 color: #FF4612;
                 background-color: #FF4612;
             }
-            input{
+
+            input {
                 border: none;
                 border-bottom: 2px solid #C0C0C0;
             }
-            textarea{
+
+            textarea {
                 border: none;
                 border-bottom: 2px solid #C0C0C0;
             }
-            .purchase{
+
+            .purchase {
                 background-color: #FF4612;
             }
-            .pointer{
+
+            .pointer {
                 cursor: pointer;
             }
         }
-        .row-images{
-            
-            .col-20{
+
+        .row-images {
+            .col-20 {
                 width: 20%;
 
-                .fa-arrow-up-long{
+                .fa-arrow-up-long {
                     right: -330px;
                     top: 0px;
                     font-size: 30px;
                     background-color: white;
                     padding: 10px;
                     border-radius: 100%;
-                    box-shadow: 5px 10px 8px  #888888;
+                    box-shadow: 5px 10px 8px #888888;
                     color: #FF4612;
                 }
             }
---
+++ src/components/testimonialsCarousel.vue
@@ -0,0 +1,177 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        <div class="container-seventh-section position-relative">
+            <a href="#"><i class="fa-solid fa-arrow-up-long position-absolute"></i></a>
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="position-relative"><strong>Testimonials.</strong></h2>
+
+                    <!-- Carosello -->
+                    <div id="testimonialCarousel" class="carousel slide position-relative">
+                        <div class="carousel-inner">
+                            <!-- Primo profilo -->
+                            <div class="carousel-item active">
+                                <div class="d-flex flex-column align-items-center text-center">
+                                    <img src="../assets/h3-img-04.png" class="d-block rounded-circle" alt="Testimonial 1">
+                                    <p class="fw-bold fs-4 mt-4">Cynthia Clark</p>
+                                    <p class="lorem fs-5">"lorem ipsum dolor sit amet, consectetur adipiscing elit sed eiusmod <br> tempor incididunt ut labore et dolore magna aliqua."</p>
+                                    <div class="progress-bar-container">
+                                        <div class="progress-bar">
+                                            <div class="progress" style="width: 33%;"></div>
+                                        </div>
+                                        <span class="progress-label start">01</span>
+                                        <span class="progress-label end">03</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Secondo profilo -->
+                            <div class="carousel-item">
+                                <div class="d-flex flex-column align-items-center text-center">
+                                    <img src="../assets/h3-img-07.png" class="d-block rounded-circle" alt="Testimonial 2">
+                                    <p class="fw-bold fs-4 mt-4">John Doe</p>
+                                    <p class="lorem fs-5">"lorem ipsum dolor sit amet, consectetur adipiscing elit sed eiusmod <br> tempor incididunt ut labore et dolore magna aliqua."</p>
+                                    <div class="progress-bar-container">
+                                        <div class="progress-bar">
+                                            <div class="progress" style="width: 66%;"></div>
+                                        </div>
+                                        <span class="progress-label start">01</span>
+                                        <span class="progress-label end">03</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Terzo profilo -->
+                            <div class="carousel-item">
+                                <div class="d-flex flex-column align-items-center text-center">
+                                    <img src="../assets/h3-img-08.png" class="d-block rounded-circle" alt="Testimonial 3">
+                                    <p class="fw-bold fs-4 mt-4">Jane Smith</p>
+                                    <p class="lorem fs-5">"lorem ipsum dolor sit amet, consectetur adipiscing elit sed eiusmod <br> tempor incididunt ut labore et dolore magna aliqua."</p>
+                                    <div class="progress-bar-container">
+                                        <div class="progress-bar">
+                                            <div class="progress" style="width: 100%;"></div>
+                                        </div>
+                                        <span class="progress-label start">01</span>
+                                        <span class="progress-label end">03</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#testimonialCarousel" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#testimonialCarousel" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.container-seventh-section {
+    height: 500px;
+    background-color: #1E1E1E;
+    padding: 0;
+    position: relative;
+
+    .fa-arrow-up-long {
+        right: 20px;
+        top: 80px;
+        font-size: 30px;
+        background-color: white;
+        padding: 10px;
+        border-radius: 100%;
+        box-shadow: 5px 10px 8px #888888;
+        color: #FF4612;
+        z-index: 10000;
+    }
+
+    h2 {
+        font-size: 200px;
+        text-align: center;
+        margin-top: -50px;
+    }
+
+    .carousel {
+        top: -130px;
+        .carousel-inner {
+            .carousel-item {
+                p {
+                    color: #ccc;
+                }
+                .d-flex {
+                    flex-direction: column;
+                    align-items: center;
+                    text-align: center;
+
+                    img {
+                        max-width: 150px;
+                        margin-bottom: 1rem;
+                    }
+
+                    .fw-bold {
+                        font-size: 1.5rem;
+                    }
+
+                    .lorem {
+                        color: #BDBDBD;
+                    }
+
+                    .progress-bar-container {
+                        width: 80%;
+                        max-width: 300px;
+                        position: relative;
+                        margin-top: 1rem;
+                    }
+
+                    .progress-bar {
+                        width: 100%;
+                        background-color: #BDBDBD;
+                        height: 4px;
+                        border-radius: 4px;
+                        position: relative;
+                    }
+
+                    .progress {
+                        background-color: #888888;
+                        height: 100%;
+                        border-radius: 4px;
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                    }
+
+                    // Labels for progress bar
+                    .progress-label {
+                        position: absolute;
+                        color: #ccc;
+                        font-size: 0.8rem;
+                        top: -50%;
+                        transform: translateY(-50%);
+                    }
+
+                    .progress-label.start {
+                        left: -2rem; // Adjust as needed
+                    }
+
+                    .progress-label.end {
+                        right: -2rem; // Adjust as needed
+                    }
+                }
+            }
+        }
+
+        .carousel-caption {
+            bottom: 20px;
+        }
+    }
+}
+</style>
